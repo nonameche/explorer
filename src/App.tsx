44b30b593879546636c0d189669e1ba9b91d622e
nonameche <946145605@qq.com>
2021-01-17 22:08:06 +0800

    ➕ add:添加classnames包 🚧 dev:开发button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react"
+import Button, { ButtonSize, ButtonType } from "./components/Button/button"
 
 function App() {
   return (
     <div className="App">
+      <Button>hello</Button>
+      <Button btnType={ButtonType.Link} href="www.baidu.com">
+        hello
+      </Button>
+      <Button btnType={ButtonType.Parmary}>hello</Button>
       <h1>111</h1>
       <h2>1111</h2>
       <h3>ere</h3>
