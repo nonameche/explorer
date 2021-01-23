06849f43147b1252aa291923f0ab3ccdec155ec0
nonameche <946145605@qq.com>
2021-01-23 23:20:10 +0800

    🚧 add:添加button组件示例

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from "@storybook/react/types-6-0"
+
+import { Button, ButtonProps } from "./button"
+
+export default {
+  title: "Example/Button",
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as Meta
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {
+  btnType: "primary",
+  children: "Button",
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  btnType: "default",
+}
+
+export const Danger = Template.bind({})
+Danger.args = {
+  btnType: "danger",
+}
+
+export const Link = Template.bind({})
+Link.args = {
+  btnType: "link",
+}
