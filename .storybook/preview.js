7a522236edcfc02ac3df4fb519d6925efc3e38d7
nonameche <946145605@qq.com>
2021-01-23 23:17:36 +0800

    🔧 modify:修改storybook 配置文件

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,20 @@
+import "../src/styles/index.scss"
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  locale: {
+    name: 'Locale',
+    description: 'Internationalization locale',
+    defaultValue: 'en',
+    toolbar: {
+      icon: 'globe',
+      items: [
+        { value: 'en', right: '🇺🇸', title: 'English' },
+        { value: 'fr', right: '🇫🇷', title: 'Français' },
+        { value: 'es', right: '🇪🇸', title: 'Español' },
+        { value: 'zh', right: '🇨🇳', title: '中文' },
+        { value: 'kr', right: '🇰🇷', title: '한국어' },
+      ],
+    },
+  },
 }
