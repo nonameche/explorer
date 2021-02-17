9d784387cc671f5ac8aa45fee248adcf1ca5548a
nonameche <946145605@qq.com>
2021-02-17 20:47:56 +0800

    修改配置文件

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   "stories": [
-    "../src/components/**/*.stories.mdx",
-    "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,20 +1,4 @@
-import "../src/styles/index.scss"
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-  locale: {
-    name: 'Locale',
-    description: 'Internationalization locale',
-    defaultValue: 'en',
-    toolbar: {
-      icon: 'globe',
-      items: [
-        { value: 'en', right: '🇺🇸', title: 'English' },
-        { value: 'fr', right: '🇫🇷', title: 'Français' },
-        { value: 'es', right: '🇪🇸', title: 'Español' },
-        { value: 'zh', right: '🇨🇳', title: '中文' },
-        { value: 'kr', right: '🇰🇷', title: '한국어' },
-      ],
-    },
-  },
 }
