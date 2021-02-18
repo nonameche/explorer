846a2430e4943fc3f23ca6e3d8c39545acc2736c
nonameche <946145605@qq.com>
2021-02-18 19:21:29 +0800

    🔧 添加scss打包配置

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,37 @@
+const path = require('path');
 module.exports = {
   "stories": [
-    "../src/components/**/*.stories.mdx",
-    "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
+    // {
+    //   name: '@storybook/preset-scss',
+    //   options: {
+    //     cssLoaderOptions: {
+    //       modules: true,
+    //     },
+    //     sassLoaderOptions: {
+    //       sourceMap: true,
+    //     }
+    //   },
+    // },
+  ],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    // Return the altered config
+    return config;
+  },
 }
