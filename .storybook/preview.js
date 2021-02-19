973988090a53abe55801a71f4a7ebda45bec9daf
nonameche <946145605@qq.com>
2021-02-19 19:04:27 +0800

    🐛 fix:解决文档图标无法加载的问题

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
 import '../src/styles/index.scss'
-
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
