6045db11bd2111b72952965dc305fb1b62ffc125
nonameche <946145605@qq.com>
2021-02-23 22:11:59 +0800

    📝 修改部分文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from "react"
 import Button from "./components/Button/button"
-import Alert, { AlertType } from "./components/Alert/alert"
+import input from "./components/Input/input"
+import Upload from "./components/Upload/upload"
+
+import Alert from "./components/Alert/alert"
+import Icon from "./components/Icon/icon"
 
 function App() {
   return (
@@ -11,9 +15,12 @@ function App() {
       </Button>
       <Button btnType="danger">hello</Button>
       {/* <Alert alertType={AlertType.Danger}>hello</Alert> */}
-      <Alert alertType={AlertType.Warning} title="嘿嘿额" showClose={true}>
+      <Alert alertType="warning" title="嘿嘿额" showClose={true}>
         hello
       </Alert>
+      <input></input>
+      <Upload action="www.baidu.com">上传</Upload>
+      <Icon icon="upload" size="5x" theme="secondary" />
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./styles/index.scss"
 import App from "./App"
-import reportWebVitals from "./reportWebVitals"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fas } from "@fortawesome/free-solid-svg-icons"
+library.add(fas)
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +16,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
