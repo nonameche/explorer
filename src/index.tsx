39c0e364acc40fb3bebae3fa46650e160b43a4ec
nonameche <946145605@qq.com>
2021-02-24 20:54:43 +0800

    ✨ 添加返回组件配置

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,9 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./styles/index.scss"
-import App from "./App"
-import { library } from "@fortawesome/fontawesome-svg-core"
-import { fas } from "@fortawesome/free-solid-svg-icons"
-library.add(fas)
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+export { default as Button } from "./components/Button/button"
+export { default as Alert } from "./components/Alert/alert"
+export { default as AutoComplete } from "./components/AutoComplete/autoComplete"
+export { default as Icon } from "./components/Icon/icon"
+export { default as Input } from "./components/Input/input"
+export { default as Menu } from "./components/Menu/menu"
+export { default as Progress } from "./components/Progress/progress"
+export { default as Transition } from "./components/Transition/transition"
+export { default as Upload } from "./components/Upload/upload"
